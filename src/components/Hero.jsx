48d81e76c67087b443d5bdc72d462147c7d89be2
atmo103 <atmo103.dev@gmail.com>
2025-10-21 12:08:27 -0500

    implement Cocktails section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,14 @@
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react"
 import { SplitText } from "gsap/all";
+import { useMediaQuery } from "react-responsive";
+import { useRef } from "react";
 
 
 const Hero = () => {
+
+    const videoRef = useRef();
+    const isMobile = useMediaQuery({ maxWidth: 767 });
 
     useGSAP(() => {
         const heroSplit = new SplitText('.title', { type: 'chars, words' });
@@ -35,46 +40,78 @@ const Hero = () => {
                 scrub: true,
             }
         })
-        .to('.right-leaf', {y: 200}, 0)
-        .to('.left-leaf', {y: -200}, 0); 
+            .to('.right-leaf', { y: 200 }, 0)
+            .to('.left-leaf', { y: -200 }, 0)
+
+        const startValue = isMobile ? "top 50%" : "center 60%";
+        const endValue = isMobile ? "120% top" : 'bottom top'
+
+        let tl = gsap.timeline({
+            scrollTrigger: {
+                trigger: "video",
+                start: startValue,
+                end: endValue,
+                scrub: true,
+                pin: true,
+            }
+        });
+
+        videoRef.current.onloadedmetadata = () => {
+            tl.to(videoRef.current, {
+                currentTime: videoRef.current.duration,
+            });
+        };
 
     }, []);
 
     return (
-        <section id="hero" className="noisy">
-            <h1 className="title">MOJITO</h1>
-            <img
-                src="/images/hero-left-leaf.png"
-                alt="left-leaf"
-                className="left-leaf"
-            />
-            <img
-                src="/images/hero-right-leaf.png"
-                alt="right-leaf"
-                className="right-leaf"
-            />
-            <div className="body">
-                <div className="content">
+        <>
+            <section id="hero" className="noisy">
+                <h1 className="title">MOJITO</h1>
+                <img
+                    src="/images/hero-left-leaf.png"
+                    alt="left-leaf"
+                    className="left-leaf"
+                />
+                <img
+                    src="/images/hero-right-leaf.png"
+                    alt="right-leaf"
+                    className="right-leaf"
+                />
+                <div className="body">
+                    <div className="content">
 
-                    <div className="space-y-5 hidden md:block">
-                        <p>Cool. Crisp. Classic.</p>
-                        <p className="subtitle">
-                            Sip the Spirit <br /> of Summer
-                        </p>
+                        <div className="space-y-5 hidden md:block">
+                            <p>Cool. Crisp. Classic.</p>
+                            <p className="subtitle">
+                                Sip the Spirit <br /> of Summer
+                            </p>
+                        </div>
+
+                        <div className="view-cocktails">
+                            <p className="subtitle">
+                                Every cocktail we serve is a reflection of our obsession with detail — from the first muddle to the final garnish. That care is what turns a simple drink into something truly memorable.
+                            </p>
+                            <a href="cocktails"> View Cocktails</a>
+                        </div>
+
+
                     </div>
-
-                    <div className="view-cocktails">
-                        <p className="subtitle">
-                            Every cocktail we serve is a reflection of our obsession with detail — from the first muddle to the final garnish. That care is what turns a simple drink into something truly memorable.
-                        </p>
-                        <a href="cocktails"> View Cocktails</a>
-                    </div>
-
-
                 </div>
-            </div>
 
-        </section>
+            </section>
+
+            <div className='video absolute inset-0'>
+                <video
+                    ref={videoRef}
+                    src="/videos/output.mp4"
+                    muted
+                    playsInline
+                    preload="auto"
+                />
+
+            </div>
+        </>
     )
 }
 
